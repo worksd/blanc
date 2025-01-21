f34c722585db4613c1ed5413bed2a1182c949f76
이동호 <dongho.lee11@cj.net>
2025-01-21 11:15:21 +0900

    1.0(3) 버전 배포

--- app/src/main/java/com/worksd/blanc/util/KloudWebUrlProvider.kt
+++ app/src/main/java/com/worksd/blanc/util/KloudWebUrlProvider.kt
@@ -3,7 +3,8 @@ package com.worksd.blanc.util
 import android.content.Context
 
 object KloudWebUrlProvider {
+    private const val DEFAULT_PRODUCTION_URL = "https://kloud-alpha.vercel.app"
     fun getUrl(context: Context, route: String): String {
-        return "${PrefUtils(context).getString(WebEndPointKey).orEmpty()}$route"
+        return "${PrefUtils(context).getString(WebEndPointKey) ?: DEFAULT_PRODUCTION_URL}$route"
     }
 }
