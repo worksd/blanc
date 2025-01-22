86993179dff668900555c0b76edac4253e88998c
이동호 <dongho.lee11@cj.net>
2025-01-22 10:35:31 +0900

    Manifest 광고 권한 삭제

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.worksd.blanc"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
