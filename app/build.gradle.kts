4bde131520c46eeb8d223a25372af2493a814f56
이동호 <dongho.lee11@cj.net>
2025-01-22 10:18:51 +0900

    1.0.0 배포

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
