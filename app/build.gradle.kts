222d84d1a236e996287bd49aed61cd469aa42874
이동호 <dongho.lee11@cj.net>
2025-09-05 18:59:22 +0900

    1.0.14 버전(안드로이드 Safe Area 대응)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.rawgraphy.blanc"
         minSdk = 26
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.0.13"
+        versionCode = 14
+        versionName = "1.0.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
